4fe077f96bac67e7b62f8dfc6dcbfa9cb6b6c271
Arif Sinan Çeçen <34174457+arifsinancecen@users.noreply.github.com>
2021-11-09 11:14:08 +0300

    Create Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,7 @@
+class Singleton { private static Singleton instance; 
+private Singleton() {
+ } public static synchronized Singleton getInstance() 
+{ if (instance == null) instance = new Singleton(); 
+return instance;
+ } public void doSomething() 
+{ } }
